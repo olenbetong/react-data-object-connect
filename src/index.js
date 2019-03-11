c278f61f3cc484d043b6d0b32f245bd0184255f2
Bjørnar Vister Hansen <bvh@olenbetong.no>
2019-03-11 09:22:24 +0100

    Restructure dist folder

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { dataObjectConnect, properties } from "./data-object-connect";
+
+export const connect = dataObjectConnect;
+export function connecRow(dataObject) {
+  return connect.dataObjectConnect(dataObject, true);
+}
+
+export const connectedProperties = properties;
+
+export function dataObjectConnectfunction() {
+  // eslint-disable-next-line no-console
+  console.warn(
+    "DEPRECATED: dataObjectConnect has been renamed to connect. The dataObjectConnect will be remove in the future."
+  );
+  return connect.dataObjectConnect.apply(this, arguments);
+}
+
+export {
+  getData,
+  useCurrentIndex,
+  useCurrentRow,
+  useData,
+  useDataWithoutState,
+  useDirty,
+  useError,
+  useFetchData,
+  useFetchRecord,
+  useLoading,
+  usePermissions,
+  useSingleRecord,
+  useStatus
+} from "./data-object-hooks";

fae0619d59421688cac6cf65ec8defb9ee7d9b7c
Bjørnar Vister Hansen <bvh@olenbetong.no>
2019-06-03 13:40:49 +0200

    Add useCallback to rollup-plugin-virtual

--- rollup.config.js
+++ rollup.config.js
@@ -80,6 +80,7 @@ function getConfig(isProd, format) {
 export default React;
 export const PureComponent = React.PureComponent;
 export const Component = React.Component;
+export const useCallback = React.useCallback;
 export const useEffect = React.useEffect;
 export const useState = React.useState;
 export const useRef = React.useRef;`

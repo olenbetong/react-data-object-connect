1b50484ec49c87a5d46d6187400af9a763915a90
Bjørnar Vister Hansen <bvh@olenbetong.no>
2019-09-09 09:26:12 +0200

    Fix build warnings

--- rollup.config.js
+++ rollup.config.js
@@ -1,97 +1,113 @@
 /* eslint-env node */
 import { appframe } from "./package.json";
 
-/* Rollup & plugins */
 import babel from "rollup-plugin-babel";
-import { terser } from "rollup-plugin-terser";
 import virtual from "rollup-plugin-virtual";
+import resolve from "rollup-plugin-node-resolve";
+import fileSize from "rollup-plugin-filesize";
+import externals from "rollup-plugin-node-externals";
+import commonjs from "rollup-plugin-commonjs";
 import replace from "rollup-plugin-replace";
+import { terser } from "rollup-plugin-terser";
 
-function getBabelConfig(format) {
+const extensions = [".js", ".jsx", ".ts", ".tsx", ".mjs", ".json"];
+
+const developmentBrowsers = [
+  "last 5 Chrome versions",
+  "last 5 Firefox versions",
+  "last 3 Safari versions",
+  "last 2 Edge versions",
+];
+
+const developmentTargets = {
+  browsers: developmentBrowsers,
+};
+
+const productionTargets = {
+  browsers: [...developmentBrowsers, "IE 11"],
+};
+
+function getBabelConfig(targets) {
   const config = {
     babelrc: false,
+    runtimeHelpers: true,
+    exclude: "node_modules/**",
+    extensions,
     presets: [
-      [
-        "@babel/preset-env",
-        {
-          targets: {
-            browsers: [
-              "last 5 Chrome versions",
-              "last 5 Firefox versions",
-              "last 3 Safari versions",
-              "last 2 Edge versions"
-            ]
-          },
-          useBuiltIns: false
-        }
-      ],
-      "@babel/preset-react"
+      ["@babel/preset-env", { targets, useBuiltIns: false }],
+      "@babel/preset-typescript",
+      "@babel/preset-react",
     ],
-    plugins: ["@babel/plugin-proposal-class-properties"],
-    ignore: ["node_modules"]
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      ["@babel/plugin-transform-runtime", { regenerator: true, useESModules: true }],
+    ],
   };
-
-  if (["umd", "iife"].includes(format)) {
-    config.presets[0][1].targets.browsers.push("IE 11");
-  } else if (format === "cjs") {
-    config.presets[0][1].targets.browsers.push("maintained node versions");
-  }
 
   return config;
 }
 
-function getConfig(isProd, format) {
-  if (isProd) {
-    process.env.NODE_ENV = "production";
-  }
+function getConfig({ isProd, format, targets = productionTargets }) {
+  const babelConfig = getBabelConfig(targets);
 
   const plugins = [
-    replace({
-      "process.env.NODE_ENV": JSON.stringify("production")
+    externals({
+      include: ["@olenbetong/common", "xdate", "react", "react-dom"],
     }),
-    babel(getBabelConfig(format))
+    babel(babelConfig),
+    commonjs(),
+    resolve({
+      extensions,
+    }),
+    replace({
+      "process.env.NODE_ENV": `"${isProd ? "production" : "development"}"`,
+    }),
+    fileSize({ showBrotli: true }),
   ];
 
   if (isProd) {
+    process.env.NODE_ENV = "production";
     plugins.push(terser());
   }
 
   const entries = appframe instanceof Array ? appframe : [appframe];
 
   return entries.map(entry => {
-    const fileExt = isProd ? `min.js` : `js`;
-    const config = {
+    const fileExt = isProd ? "min.js" : "js";
+    const conf = {
       input: `src/${entry.fileName}.js`,
       plugins,
       output: {
-        externals: ["react", "react-dom"],
-        file: `dist/${format}/${entry.fileName}.${fileExt}`,
-        format: format,
+        file: `dist/${format}/${entry.libraryName}.${fileExt}`,
+        format,
+        name: entry.libraryName,
         globals: {
           react: "React",
-          "react-dom": "ReactDOM"
+          "react-dom": "ReactDOM",
+          "@olenbetong/common": "af.common",
+          xdate: "XDate",
         },
-        name: entry.libraryName
-      }
+      },
     };
 
     if (format === "esm") {
-      config.plugins.push(
+      conf.plugins.push(
         virtual({
+          xdate: `const { XDate } = window; export default XDate;`,
           "react-dom": `const { ReactDOM } = window; export default ReactDOM;`,
           react: `const { React } = window;
-export default React;
-export const PureComponent = React.PureComponent;
-export const Component = React.Component;
-export const useCallback = React.useCallback;
-export const useEffect = React.useEffect;
-export const useState = React.useState;
-export const useRef = React.useRef;`
-        })
+  export default React;
+  export const PureComponent = React.PureComponent;
+  export const Component = React.Component;
+  export const useCallback = React.useCallback;
+  export const useEffect = React.useEffect;
+  export const useState = React.useState;
+  export const useRef = React.useRef;`,
+        }),
       );
     }
 
-    return config;
+    return conf;
   });
 }
 
@@ -99,9 +115,9 @@ module.exports = commandLineArgs => {
   const isProd = commandLineArgs.configProd === true;
 
   return [
-    ...getConfig(isProd, "esm"),
-    ...getConfig(isProd, "umd"),
-    ...getConfig(isProd, "iife"),
-    ...getConfig(isProd, "cjs")
+    ...getConfig({ isProd, format: "esm", targets: developmentTargets }),
+    ...getConfig({ isProd, format: "umd", targets: productionTargets }),
+    ...getConfig({ isProd, format: "iife", targets: productionTargets }),
+    ...getConfig({ isProd, format: "cjs", targets: { node: "9" } }),
   ];
 };

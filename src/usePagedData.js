9cb5a7602ebc0499bd825b1a3dbb2caf5eddab8b
Bjørnar Vister Hansen <bvh@olenbetong.no>
2019-09-23 12:20:04 +0200

    Fix

--- src/usePagedData.js
+++ src/usePagedData.js
@@ -1,26 +1,29 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import dataUpdateEvents from "./dataUpdateEvents";
 
+function getCurrentData(dataObject) {
+  const newData = dataObject.getPagingComponent().getCurrentData();
+
+  // If current row is dirty, getData will still return the saved record
+  const idx = dataObject.getCurrentIndex();
+  if (idx >= 0) {
+    newData[idx] = dataObject.currentRow();
+  }
+
+  return newData || [];
+}
+
 export default function usePagedData(dataObject) {
-  const [data, setData] = useState(() => dataObject.getPagingComponent().getCurrentData() || []);
-
-  const updateData = useCallback(() => {
-    const data = dataObject.getPagingComponent().getCurrentData();
-
-    // If current row is dirty, getData will still return the saved record
-    const idx = dataObject.getCurrentIndex();
-    if (idx >= 0) {
-      data[idx] = dataObject.currentRow();
-    }
-
-    setData(data || []);
-  }, [dataObject]);
+  const [currentData, setCurrentData] = useState(() => getCurrentData(dataObject));
 
   useEffect(() => {
+    function updateData() {
+      setCurrentData([...getCurrentData(dataObject)]);
+    }
+
     dataUpdateEvents.forEach(event => dataObject.attachEvent(event, updateData));
 
     const pagingComponent = dataObject.getPagingComponent();
-    pagingComponent.attach("after", "pageCountChange", updateData);
     pagingComponent.attach("after", "pageChange", updateData);
     pagingComponent.attach("after", "pageRefresh", updateData);
 
@@ -28,11 +31,10 @@ export default function usePagedData(dataObject) {
 
     return () => {
       dataUpdateEvents.forEach(event => dataObject.detachEvent(event, updateData));
-      pagingComponent.detach("after", "pageCountChange", updateData);
       pagingComponent.detach("after", "pageChange", updateData);
       pagingComponent.detach("after", "pageRefresh", updateData);
     };
-  }, [dataObject, updateData]);
+  }, [dataObject]);
 
-  return data;
+  return currentData;
 }

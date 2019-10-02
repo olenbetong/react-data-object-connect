5362885d2ee9aabfba47d188d0d95b8f9d3a74c8
Bjørnar Vister Hansen <bvh@olenbetong.no>
2019-10-02 09:32:19 +0200

    Fix equality check of parameters in useProcedure

--- src/useProcedure.js
+++ src/useProcedure.js
@@ -1,37 +1,46 @@
 import { useEffect, useRef, useState } from "react";
 import equals from "fast-deep-equal";
 
+function useDeepCompareMemoize(value) {
+  const ref = useRef();
+
+  if (!equals(value, ref.current)) {
+    ref.current = value;
+  }
+
+  return ref.current;
+}
+
+export function useDeepCompareEffect(callback, dependencies) {
+  useEffect(callback, useDeepCompareMemoize(dependencies));
+}
+
 export default function useProcedure(procedure, params) {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [isExecuting, setIsExecuting] = useState(false);
-  const lastParams = useRef(null);
 
-  useEffect(() => {
-    let isAborted = true;
+  useDeepCompareEffect(() => {
+    let isAborted = false;
 
     if (params && procedure) {
-      if (!equals(lastParams.current, params)) {
-        lastParams.current = params;
-
-        setIsExecuting(true);
-        procedure.execute(params, (err, data) => {
-          if (isAborted) {
-            setIsExecuting(false);
-            if (err) {
-              setError(err);
-              setData([]);
-            } else {
-              setData(data);
-              setError(null);
-            }
+      setIsExecuting(true);
+      procedure.execute(params, (err, data) => {
+        if (!isAborted) {
+          setIsExecuting(false);
+          if (err) {
+            setError(err);
+            setData([]);
+          } else {
+            setData(data);
+            setError(null);
           }
-        });
-      }
+        }
+      });
     }
 
     return () => {
-      isAborted = false;
+      isAborted = true;
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [procedure, params]);

a32eabc7a497f8875f55980a87ccb43a67ad2b0b
Bjørnar Vister Hansen <bvh@olenbetong.no>
2019-10-02 09:03:45 +0200

    Use fast-deep-equal to compare parameters

--- src/useProcedure.js
+++ src/useProcedure.js
@@ -1,36 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import equals from "fast-deep-equal";
 
 export default function useProcedure(procedure, params) {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [isExecuting, setIsExecuting] = useState(false);
-
-  const deps = typeof params === "object" ? [procedure, ...Object.values(params)] : [procedure, params];
+  const lastParams = useRef(null);
 
   useEffect(() => {
     let isAborted = true;
 
     if (params && procedure) {
-      setIsExecuting(true);
-      procedure.execute(params, (err, data) => {
-        if (isAborted) {
-          setIsExecuting(false);
-          if (err) {
-            setError(err);
-            setData([]);
-          } else {
-            setData(data);
-            setError(null);
+      if (!equals(lastParams.current, params)) {
+        lastParams.current = params;
+
+        setIsExecuting(true);
+        procedure.execute(params, (err, data) => {
+          if (isAborted) {
+            setIsExecuting(false);
+            if (err) {
+              setError(err);
+              setData([]);
+            } else {
+              setData(data);
+              setError(null);
+            }
           }
-        }
-      });
+        });
+      }
     }
 
     return () => {
       isAborted = false;
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, deps);
+  }, [procedure, params]);
 
   return { data, error, isExecuting };
 }

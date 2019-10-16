5200a9f23cc041c1d546976d74d9d80df491a477
Bjørnar Vister Hansen <bvh@olenbetong.no>
2019-10-16 10:19:03 +0200

    Merge useData and usePagedData logic

--- src/useData.js
+++ src/useData.js
@@ -1,9 +1,10 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import dataUpdateEvents from "./dataUpdateEvents";
 
-export default function useData(dataObject) {
-  const [data, setData] = useState(dataObject.getData());
-  const updateData = useCallback(() => {
+function getCurrentData(dataObject) {
+  if (dataObject.isDynamicLoading()) {
+    return dataObject.getPagingComponent().getCurrentData() || [];
+  } else {
     const data = dataObject.getData();
 
     // If current row is dirty, getData will still return the saved record
@@ -12,16 +13,46 @@ export default function useData(dataObject) {
       data[idx] = dataObject.currentRow();
     }
 
-    setData(data);
-  }, [dataObject]);
+    return data;
+  }
+}
+
+export default function useData(dataObject) {
+  const [data, setData] = useState(getCurrentData());
 
   useEffect(() => {
+    const pagingComponent = dataObject.isDynamicLoading() && dataObject.getPagingComponent();
+
+    function updateData() {
+      if (pagingComponent) {
+        const page = pagingComponent.getCurrentPage();
+        if (pagingComponent.loadedPages()[page]) {
+          setData([...getCurrentData(dataObject)]);
+        }
+      } else {
+        setData([...getCurrentData(dataObject)]);
+      }
+    }
+
     dataUpdateEvents.forEach(event => dataObject.attachEvent(event, updateData));
+
+    if (dataObject.isDynamicLoading()) {
+      pagingComponent.attach("on", "pageChange", updateData);
+      pagingComponent.attach("on", "pageRefresh", updateData);
+    }
 
     updateData();
 
-    return () => dataUpdateEvents.forEach(event => dataObject.detachEvent(event, updateData));
-  }, [dataObject, updateData]);
+    return () => {
+      dataUpdateEvents.forEach(event => dataObject.detachEvent(event, updateData));
+
+      if (dataObject.isDynamicLoading()) {
+        const pagingComponent = dataObject.getPagingComponent();
+        pagingComponent.detach("on", "pageChange", updateData);
+        pagingComponent.detach("on", "pageRefresh", updateData);
+      }
+    };
+  }, [dataObject]);
 
   return data;
 }

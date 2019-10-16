927c6430884c49ccc58c1cd6ffb655f1329999ac
Bjørnar Vister Hansen <bvh@olenbetong.no>
2019-10-16 10:27:01 +0200

    Merge useData and usePagedData

--- src/usePagedDataWithFilter.js
+++ src/usePagedDataWithFilter.js
@@ -1,9 +1,1 @@
-import usePagedData from "./usePagedData";
-import useFilter from "./useFilter";
-
-export default function usePagedDataWithFilter(dataObject, filter, type = "filterString") {
-  const data = usePagedData(dataObject);
-  useFilter(dataObject, filter, type);
-
-  return data;
-}
+export { default } from "./useDataWithFilter";

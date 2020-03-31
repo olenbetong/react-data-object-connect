40c8b8148bc714245078f5dc208eb76b4a0011f5
Bjørnar Vister Hansen <bjornarvh@gmail.com>
2020-03-31 15:58:25 +0200

    Support paging in data object > 0.6.0

--- src/usePaging.js
+++ src/usePaging.js
@@ -5,17 +5,28 @@ export default function usePaging(dataObject) {
   const [pageCount, setPageCount] = useState(() => dataObject.getPagingComponent().getPageCount());
 
   useEffect(() => {
-    const paging = dataObject.getPagingComponent();
+    let [major, minor] = (dataObject.version ?? "0.6.0").split(".");
+    let paging = dataObject.getPagingComponent();
 
-    const updateCurrentPage = () => setPage(paging.getCurrentPage());
-    const updatePageCount = () => setPageCount(paging.getPageCount());
+    let updateCurrentPage = () => setPage(paging.getCurrentPage());
+    let updatePageCount = () => setPageCount(paging.getPageCount());
 
-    paging.attach("on", "pageChange", updateCurrentPage);
-    paging.attach("on", "pageCountChange", updatePageCount);
+    if (major === "0" && Number(minor) <= 6) {
+      paging.attach("on", "pageChange", updateCurrentPage);
+      paging.attach("on", "pageCountChange", updatePageCount);
+    } else {
+      paging.attach("pageChange", updateCurrentPage);
+      paging.attach("pageCountChange", updatePageCount);
+    }
 
     return () => {
-      paging.detach("on", "pageChange", updateCurrentPage);
-      paging.detach("on", "pageCountChange", updatePageCount);
+      if (major === "0" && Number(minor) <= 6) {
+        paging.detach("on", "pageChange", updateCurrentPage);
+        paging.detach("on", "pageCountChange", updatePageCount);
+      } else {
+        paging.detach("pageChange", updateCurrentPage);
+        paging.detach("pageCountChange", updatePageCount);
+      }
     };
   }, [dataObject]);
 

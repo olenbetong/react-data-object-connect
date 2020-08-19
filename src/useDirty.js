b8d4c0c64c64e38cdc4e294fe4d8e630d5ec58a6
Bjørnar Vister Hansen <bvh@olenbetong.no>
2020-08-19 14:04:43 +0200

    Fix useDirty for newer data object versions

--- src/useDirty.js
+++ src/useDirty.js
@@ -4,12 +4,25 @@ export default function useDirty(dataObject) {
   const [isDirty, setDirty] = useState(dataObject.isDirty() || false);
 
   useEffect(() => {
-    dataObject.attachEvent("onDirtyChanged", setDirty);
+    dataObject.attachEvent("onDirtyChanged", handleDirtyChanged);
 
     setDirty(dataObject.isDirty());
 
+    function handleDirtyChanged(dirtyOrEvent) {
+      // Previous versions of data object passed only the boolean
+      // value to the event handler, but newer versions pass a CustomEvent
+      // with the value in detail property.
+      if (typeof dirtyOrEvent === "boolean") {
+        setDirty(dirtyOrEvent);
+      } else if (dirtyOrEvent && dirtyOrEvent.detail) {
+        setDirty(dirtyOrEvent.detail);
+      } else {
+        setDirty(dataObject.isDirty());
+      }
+    }
+
     return () => {
-      dataObject.detachEvent("onDirtyChanged", setDirty);
+      dataObject.detachEvent("onDirtyChanged", handleDirtyChanged);
     };
   }, [dataObject]);
 

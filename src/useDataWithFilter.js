990cd1c40d951fa23cca30d59726e5e2114665ea
Bjørnar Vister Hansen <bvh@olenbetong.no>
2020-08-19 13:08:59 +0200

    Check if second parameter is object, and use as options

--- src/useDataWithFilter.js
+++ src/useDataWithFilter.js
@@ -1,8 +1,18 @@
 import useData from "./useData";
 import useFilter from "./useFilter";
 
-export default function useDataWithFilter(dataObject, filter, type = "filterString") {
-  const data = useData(dataObject);
+export default function useDataWithFilter(dataObject, filterOrOptions, typeParam = "filterString") {
+  let filter, type, options;
+
+  if (typeof filterOrOptions === "object") {
+    filter = filterOrOptions.filter;
+    type = filterOrOptions.type ?? "filterString";
+  } else {
+    filter = filterOrOptions;
+    type = typeParam;
+  }
+
+  const data = useData(dataObject, options);
   useFilter(dataObject, filter, type);
 
   return data;

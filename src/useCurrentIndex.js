a04a7d61651be55c90863f1e202fbb1cef029b00
Bjørnar Vister Hansen <bvh@olenbetong.no>
2020-10-20 15:03:00 +0200

    Compatability fix with newer data objects

--- src/useCurrentIndex.js
+++ src/useCurrentIndex.js
@@ -4,11 +4,18 @@ export default function useCurrentIndex(dataObject) {
   const [index, setIndex] = useState(dataObject.getCurrentIndex());
 
   useEffect(() => {
-    dataObject.attachEvent("onCurrentIndexChanged", setIndex);
+    function handleCurrentIndexChanged(evtOrIndex) {
+      if (typeof evtOrIndex === "number") {
+        setIndex(evtOrIndex);
+      } else if (typeof evtOrIndex === "object" && typeof evtOrIndex.detail === "number") {
+        setIndex(evtOrIndex.detail);
+      }
+    }
+    dataObject.attachEvent("onCurrentIndexChanged", handleCurrentIndexChanged);
 
     setIndex(dataObject.getCurrentIndex());
 
-    return () => dataObject.detachEvent("onCurrentIndexChanged", setIndex);
+    return () => dataObject.detachEvent("onCurrentIndexChanged", handleCurrentIndexChanged);
   }, [dataObject]);
 
   return index;

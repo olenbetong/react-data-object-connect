f36e08ef182d36e7d93431bcfa3aef0fc0452bef
Bjørnar Vister Hansen <bjornarvh@gmail.com>
2021-05-03 22:43:00 +0200

    Revive date values in getData

--- src/getData.ts
+++ src/getData.ts
@@ -14,6 +14,19 @@ declare global {
   }
 }
 
+const isodate = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(?:\.(\d{1,7}))?Z?$/;
+function dateReviver(value: any) {
+  // New method of parsing dates - much faster
+  if (typeof value === "string") {
+    if (value.includes("/Date(")) {
+      value = new Date(Number(value.substring(6, value.length - 2)));
+    } else if (isodate.test(value)) {
+      value = new Date(value);
+    }
+  }
+  return value;
+}
+
 export default function getData<T>(dataObject: DataObject<T>, filter: any): Promise<Array<T>> {
   const { data } = window.af;
   const dataHandler: DataHandler<T> = new data.DataProviderHandler({
@@ -38,7 +51,7 @@ export default function getData<T>(dataObject: DataObject<T>, filter: any): Prom
         for (let item of data) {
           const record: Partial<T> = {};
           for (let i = 0; i < item.length; i++) {
-            record[fields[i].name as keyof T] = item[i];
+            record[fields[i].name as keyof T] = dateReviver(item[i]);
           }
           records.push(record as Required<T>);
         }
